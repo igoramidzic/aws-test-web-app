a708974046475491e2209f669d0efc22829a659c
Igor Amidzic <amidzicigor46@gmail.com>
2021-01-28 13:52:27 -0500

    change

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var name = "Igor Amidzic";
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(name)
+})
+
+app.get('/api/set-name', (req, res) => {
+  name = req.params.name
 })
 
 app.listen(port, () => {
